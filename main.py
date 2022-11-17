665e09dc496d6ca1ff2e19fe3d935b6c394928a4
swifteralex <swifteralexc@gmail.com>
2022-11-17 15:42:05 -0800

    added more user input

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
-    filename = input("Type in the name of the file to test: ")
+    filename = input("Type in the name of the file to test:\n")
     data = []
     with open(filename, "r") as file:
         for line in file:
             data.append([float(number) for number in line.split()])
-    print(data)
+
+    algorithm = input("\nType the number of the search algorithm you'd like to use:\n"
+                      " 1) Forward Selection\n"
+                      " 2) Backward Elimination\n")
+
+    print("\nThis dataset has " + str(len(data[0]) - 1) + " features with " + str(len(data)) + " instances.")
 
 
 if __name__ == "__main__":
