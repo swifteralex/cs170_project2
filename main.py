29b874a9eff949c04bc334f93d70396b370a4dd9
swifteralex <swifteralexc@gmail.com>
2022-11-17 13:57:23 -0800

    test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     f = open("CS170_Small_Data__97.txt", "r")
     print([float(number) for number in f.readline().split()])
-
+    print("test")
 
 if __name__ == "__main__":
     main()
