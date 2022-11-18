0eb512e986717816c992b08c00786f46215d1204
swifteralex <swifteralexc@gmail.com>
2022-11-17 22:02:56 -0800

    added forward elimination functionality

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import copy
+
 def k_fold_cross_validation(data, features):
     correct_count = 0
 
     for removed_row in data:
         shortest_distance = 999999999.9
-        nearest_class = 1
+        nearest_class = 1.0
         for comparison_row in data:
             if removed_row == comparison_row:
                 continue
@@ -29,13 +31,43 @@ def main():
         for line in file:
             data.append([float(number) for number in line.split()])
 
+    num_features = len(data[0]) - 1
     algorithm = input("\nType the number of the search algorithm you'd like to use:\n"
                       " 1) Forward Selection\n"
                       " 2) Backward Elimination\n")
 
-    print("\nThis dataset has " + str(len(data[0]) - 1) + " features with " + str(len(data)) + " instances.")
+    print("\nThis dataset has " + str(num_features) + " features with " + str(len(data)) + " instances.")
 
-    print(k_fold_cross_validation(data, [18, 25, 34]))
+    print("\nBeginning search...\n")
+
+    current_set = []
+    unused_features = [number + 1 for number in range(num_features)]
+    best_set_overall = []
+    best_accuracy_overall = 0.0
+
+    if algorithm == '1':
+        while len(unused_features) > 0:
+            best_set = []
+            best_accuracy = 0.0
+
+            for feature in unused_features:
+                current_set.append(feature)
+                accuracy = k_fold_cross_validation(data, current_set)
+                if accuracy > best_accuracy:
+                    best_accuracy = accuracy
+                    best_set = copy.deepcopy(current_set)
+                if accuracy > best_accuracy_overall:
+                    best_accuracy_overall = accuracy
+                    best_set_overall = copy.deepcopy(current_set)
+                print("    Using feature set " + str(current_set) + " gives an accuracy of " + str(accuracy) + ".")
+                current_set.pop()
+
+            print("\nFeature set " + str(best_set) + " was best with an accuracy of " + str(best_accuracy) + ".\n")
+            unused_features.remove(best_set[-1])
+            current_set.append(best_set[-1])
+        print("Finished search! The best set of features found was " +
+              str(best_set_overall) + " with an accuracy of " + str(best_accuracy_overall) + ".")
+
 
 
 if __name__ == "__main__":
