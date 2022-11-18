338016ececcc66696cf023c44acc0ce1f9e25e6d
swifteralex <swifteralexc@gmail.com>
2022-11-17 18:16:57 -0800

    added k-fold cross validation function to evaluate accuracy

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+def k_fold_cross_validation(data, features):
+    correct_count = 0
+
+    for removed_row in data:
+        shortest_distance = 999999999.9
+        nearest_class = 1
+        for comparison_row in data:
+            if removed_row == comparison_row:
+                continue
+
+            distance = 0
+            for feature in features:
+                distance += (removed_row[feature] - comparison_row[feature]) ** 2
+            distance = distance ** 0.5
+            if distance < shortest_distance:
+                shortest_distance = distance
+                nearest_class = comparison_row[0]
+
+        if abs(nearest_class - removed_row[0]) < 0.0001:
+            correct_count += 1
+
+    return correct_count / len(data)
+
+
 def main():
     filename = input("Type in the name of the file to test:\n")
     data = []
@@ -10,6 +34,8 @@ def main():
                       " 2) Backward Elimination\n")
 
     print("\nThis dataset has " + str(len(data[0]) - 1) + " features with " + str(len(data)) + " instances.")
+
+    print(k_fold_cross_validation(data, [18, 25, 34]))
 
 
 if __name__ == "__main__":
